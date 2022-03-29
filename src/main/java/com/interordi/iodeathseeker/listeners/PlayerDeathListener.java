0181574320a1ecdfe055e55b279719be82b877aa
Stéphane Lemay <interordi@interordi.com>
2022-03-28 23:41:20 -0400

    Remove from the message which mob caused the death

--- src/main/java/com/interordi/iodeathseeker/listeners/PlayerDeathListener.java
+++ src/main/java/com/interordi/iodeathseeker/listeners/PlayerDeathListener.java
@@ -3,9 +3,13 @@ package com.interordi.iodeathseeker.listeners;
 import com.interordi.iodeathseeker.IODeathSeeker;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerDeathListener implements Listener {
@@ -20,14 +24,32 @@ public class PlayerDeathListener implements Listener {
 	
 	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerDeathEvent(PlayerDeathEvent event) {
-		String message = event.getDeathMessage();
+		String message = event.getDeathMessage().toLowerCase();
 
 		//Remove the player's name
-		message = message.replace(event.getEntity().getDisplayName(), "").trim();
+		message = message.replace(event.getEntity().getDisplayName().toLowerCase(), "").trim();
 
 		//Replace the killer's name
-		//TODO: Better handling, some names have spaces
-		message = message.substring(0, message.lastIndexOf(" ")).trim();
+		String killerName = "";
+		EntityDamageEvent lastDamage = event.getEntity().getLastDamageCause();
+		if (lastDamage != null && lastDamage instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent)lastDamage;
+			
+			if (nEvent.getDamager() != null) {
+				//If the damage came from a projectile, find said projectile's owner
+				if (nEvent.getDamager() instanceof Projectile) {
+					final Projectile projectile = (Projectile)nEvent.getDamager();
+					Entity temp = (Entity)projectile.getShooter();
+					killerName = temp.getType().toString().toLowerCase();
+				}
+				//Just a regular mob kill
+				else {
+					killerName = nEvent.getDamager().getType().toString().toLowerCase();
+				}
+			}
+		}
+
+		message = message.replace(killerName, "").trim();
 
 		if (message.length() > 0)
 			this.plugin.players.logDeath(event.getEntity().getUniqueId(), message, 1);
