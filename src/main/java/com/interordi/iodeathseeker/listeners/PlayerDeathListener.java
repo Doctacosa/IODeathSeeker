e69fb24f8041f36b839cede745670ae5ee8ee49e
Stéphane Lemay <interordi@interordi.com>
2022-04-03 20:19:04 -0400

    Catch accidental deaths like "trying to avoid"

--- src/main/java/com/interordi/iodeathseeker/listeners/PlayerDeathListener.java
+++ src/main/java/com/interordi/iodeathseeker/listeners/PlayerDeathListener.java
@@ -1,13 +1,19 @@
 package com.interordi.iodeathseeker.listeners;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import com.interordi.iodeathseeker.IODeathSeeker;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -15,19 +21,59 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 public class PlayerDeathListener implements Listener {
 
 	private IODeathSeeker plugin;
+	private Map< UUID, String > lastMobDamage;
 	
 	public PlayerDeathListener(IODeathSeeker plugin) {
 		this.plugin = plugin;
+		lastMobDamage = new HashMap< UUID, String >();
 		Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+
+	
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		
+		//Attack from a mob or another player
+		if (event instanceof EntityDamageByEntityEvent) {
+			onEntityDamage((EntityDamageByEntityEvent)event);
+		}
+	}
+	
+	
+	public void onEntityDamage(EntityDamageByEntityEvent event) {
+		
+		float damage = Math.round(event.getFinalDamage() * 10) / 10;
+		
+		//Ignore no damage
+		if (damage == 0)
+			return;
+		
+		boolean playerTarget = (event.getEntity() != null && event.getEntity() instanceof Player);
+		
+		//No human target = don't care
+		if (!playerTarget) {
+			return;
+		}
+		
+		Entity attacker = event.getDamager();
+		Entity target = event.getEntity();
+		
+		//If the damage came from a projectile, find said projectile's owner instead
+		if (event.getDamager() instanceof Projectile) {
+			final Projectile projectile = (Projectile)event.getDamager();
+			attacker = (Entity)projectile.getShooter();
+
+		}
+
+		//This isn't perfect and doesn't need to be - at worst, it will be ignored
+		lastMobDamage.put(target.getUniqueId(), attacker.getName().toString());
 	}
 	
 	
 	@EventHandler(priority = EventPriority.LOW)
 	public void onPlayerDeathEvent(PlayerDeathEvent event) {
 		String message = event.getDeathMessage().toLowerCase();
-
-		//Remove the player's name
-		message = message.replace(event.getEntity().getDisplayName().toLowerCase(), "").trim();
 
 		//Replace the killer's name
 		String killerName = "";
@@ -47,9 +93,27 @@ public class PlayerDeathListener implements Listener {
 					killerName = cleanName(nEvent.getDamager().getType().toString());
 				}
 			}
+		} else if (lastDamage != null && lastDamage instanceof EntityDamageByBlockEvent) {
+			EntityDamageByBlockEvent nEvent = (EntityDamageByBlockEvent)lastDamage;
+
+			//Ignored, handled through the lastMobDamage map
+			if (nEvent.getDamager() != null) {
+				//System.out.println("Killer name block: " + nEvent.getDamager().getType().toString());
+			}
+
 		}
 
+		//Remove the player's name
+		message = message.replace(event.getEntity().getDisplayName().toLowerCase(), "").trim();
+
+		//Remove the killer's name
 		message = message.replace(killerName, "").trim();
+
+		//Remove the last damager's name
+		String lastMobName = lastMobDamage.get(event.getEntity().getUniqueId());
+		if (lastMobName != null)
+			message = message.replace(cleanName(lastMobName), "").trim();
+
 
 		if (message.length() > 0)
 			this.plugin.players.logDeath(event.getEntity().getUniqueId(), message, 1);
